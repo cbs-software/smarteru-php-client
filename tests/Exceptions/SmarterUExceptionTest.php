c5a3c12c61f257bf2ac9afd7d0cedc1ae2ca5c56
Brian Reich <brian.reich@thecoresolution.com>
2023-11-16 09:08:19 -0500

    chore: revert change to SmarterUExceptionTest

--- tests/Exceptions/SmarterUExceptionTest.php
+++ tests/Exceptions/SmarterUExceptionTest.php
@@ -24,8 +24,6 @@ class SmarterUExceptionTest extends TestCase {
     public function testAgreement() {
         $code = 'SU:01';
         $message = 'No POST data detected';
-        $request = '<SmarterU><AccountAPI>12345</AccountAPI><UserAPI>password</UserAPI><Method>updateUser</Method><Parameters><User><Identifier><Email>ryan.davis@thecoresolution.com</Email></Identifier><Info><Email>ryan.davis@thecoresolution.com</Email><EmployeeID/><GivenName>Ryann</GivenName></Info><Profile><Status>Active</Status><ReceiveNotifications>1</ReceiveNotifications></Profile><Groups/><Venues/><Wages/></User></Parameters></SmarterU>';
-        $response = '<SmarterU><Result>Failed</Result><Info/><Errors><Error><ErrorID>UU:69</ErrorID><ErrorMessage>The requested user cannot be updated via the API.</ErrorMessage></Error></Errors></SmarterU>';
         $errorCode = new ErrorCode($code, $message);
 
         $exception = new SmarterUException($message, [$errorCode], $request, $response);
@@ -36,10 +34,7 @@ class SmarterUExceptionTest extends TestCase {
         self::assertCount(1, $errorCodes);
 
         $errorCode = reset($errorCodes);
-        $expectedRequest = '<SmarterU><AccountAPI>********</AccountAPI><UserAPI>********</UserAPI><Method>updateUser</Method><Parameters><User><Identifier><Email>ryan.davis@thecoresolution.com</Email></Identifier><Info><Email>ryan.davis@thecoresolution.com</Email><EmployeeID/><GivenName>Ryann</GivenName></Info><Profile><Status>Active</Status><ReceiveNotifications>1</ReceiveNotifications></Profile><Groups/><Venues/><Wages/></User></Parameters></SmarterU>';
         self::assertEquals($code, $errorCode->getErrorCode());
         self::assertEquals($message, $errorCode->getErrorMessage());
-        self::assertEquals($expectedRequest, $exception->getRequest());
-        self::assertEquals($response, $exception->getResponse());
     }
 }
